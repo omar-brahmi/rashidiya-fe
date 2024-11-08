972931526fe03fd7f74c7a78f7f06e4d7bdfbbef
Omar Brahmi <omar.brahmi@outlook.be>
2024-11-08 13:21:43 +0100

    Fixed silder and SSL

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'rashidiya-fe',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    cleartext: true,
+    androidScheme: 'http'
+  }, android: {
+    allowMixedContent: true
+  }
 };
 
 export default config;
