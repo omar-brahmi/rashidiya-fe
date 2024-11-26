025662609f59be0a6c3a6f2a6317016177c427dc
Omar Brahmi <omar.brahmi@outlook.be>
2024-11-26 21:41:21 +0100

    Fixed login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,18 +7,20 @@ import {NavController} from "@ionic/angular";
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
-
   email: string = '';
   password: string = '';
+  showPassword: boolean = false;
 
   constructor(private navCtrl: NavController) {
   }
 
   signIn() {
-    // Add your login logic here
     console.log('Email:', this.email);
     console.log('Password:', this.password);
-    // Navigate to another page if needed
     this.navCtrl.navigateForward('/home');
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
   }
 }
