bfaa5206d5e2662556a5fa7deb0dec036107cd70
Omar Brahmi <omar.brahmi@outlook.be>
2024-12-06 14:51:47 +0100

    Fixed issues

--- src/app/user/user-profile/user-profile.page.ts
+++ src/app/user/user-profile/user-profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Role, User } from '../../core/models/user.model';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.page.scss'],
 })
 export class UserProfilePage implements OnInit {
+  user: User = {
+    id: 1,
+    firstname: 'Amir',
+    lastname: 'Elghribi',
+    role: Role.ADMIN,
+    username: 'amirelghribi',
+    email: 'amir.elghribi@example.com',
+    password: ''
+  };
 
-  constructor() { }
+  stats = {
+    operations: 2451,
+    clients: 1257
+  };
+
+  constructor(private navController: NavController) { }
 
   ngOnInit() {
   }
 
+  getInitials(): string {
+    return `${this.user.firstname.charAt(0)}${this.user.lastname.charAt(0)}`;
+  }
+
+  goBack() {
+    this.navController.back();
+  }
+
+  editProfile() {
+    // Implement edit profile logic
+    console.log('Edit profile clicked');
+  }
+
+  logout() {
+    // Implement logout logic
+    console.log('Logout clicked');
+  }
 }
